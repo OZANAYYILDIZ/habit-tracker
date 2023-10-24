99dc9ad7258a0a513236416da039bf6f555f25fb
Ozan Ayyıldız <ozanayyildiz92@gmail.com>
2023-10-25 00:04:20 +0300

    Progress component created and imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Wellcome from "./components/Welcome/Wellcome";
+import Progress from "./components/Progress/Progress";
 
 function App() {
   return (
     <>
       <Wellcome />
+      <Progress />
     </>
   );
 }
---
+++ src/components/Progress/Progress.jsx
@@ -0,0 +1,11 @@
+import "./Progress.css";
+
+const Progress = () => {
+  return (
+    <div>
+      <h1>Progress trackers shows here!</h1>
+    </div>
+  );
+};
+
+export default Progress;
