56538a317da546eb2e05e43a5e7870601afa76e2
Ozan Ayyıldız <ozanayyildiz92@gmail.com>
2023-10-24 23:52:20 +0300

    profile pic added, some styling made

--- src/components/Welcome/Wellcome.jsx
+++ src/components/Welcome/Wellcome.jsx
@@ -3,7 +3,14 @@ import "./Wellcome.css";
 const Wellcome = () => {
   return (
     <>
-      <div className="username">User Name</div>
+      <div className="user">
+        <img
+          className="profile-pic"
+          src="https://fastly.picsum.photos/id/780/200/200.jpg?hmac=2eohvtsHnYuBD7aalSWH4h6L6EKelvoFD9TV0Ax1K2A"
+          alt="Profile picture"
+        />
+        <h1 className="username">Ozan Ayyıldız</h1>
+      </div>
     </>
   );
 };
